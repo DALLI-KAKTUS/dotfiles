e11d411d2ed7b1444d1664e1c48773a525508513
DALLI-KAKTUS <berked2003@hotmail.com>
2025-03-29 04:41:33 +0300

    nvim changes

--- home-programs.nix
+++ home-programs.nix
@@ -311,6 +311,14 @@ in {
         config = toLua "require'alpha'.setup(require'alpha.themes.theta'.config)";
       }
       {
+        # Oil
+        plugin = oil-nvim;
+        config = toLua ''
+          local oil = require("oil")
+          oil.setup()
+          vim.keymap.set("n", "-", oil.toggle_float, {}) '';
+      }
+      {
         #Flash
         plugin = flash-nvim;
         config = toLua ''
@@ -479,24 +487,7 @@ in {
         plugin = comment-nvim;
         config = toLua ''require('Comment').setup()'';
       }
-      {
-        # spectre
-        plugin = nvim-spectre;
-        config = toLua ''
-          vim.keymap.set('n', '<leader>S', '<cmd>lua require("spectre").toggle()<CR>', {
-              desc = "Toggle Spectre"
-          })
-          vim.keymap.set('n', '<leader>sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
-              desc = "Search current word"
-          })
-          vim.keymap.set('v', '<leader>sw', '<esc><cmd>lua require("spectre").open_visual()<CR>', {
-              desc = "Search current word"
-          })
-          vim.keymap.set('n', '<leader>sp', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
-              desc = "Search on current file"
-          })
-        '';
-      }
+      
       {
         # Undotree
         plugin = undotree;
@@ -506,6 +497,21 @@ in {
         # Bufferline
         plugin = bufferline-nvim;
         config = toLua ''require('bufferline').setup({}) '';
+      }
+      {
+        # cmp-cmdline
+        plugin = cmp-cmdline;
+        config = toLua ''
+          cmp.setup.cmdline(':', {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = cmp.config.sources({
+              { name = 'path' }
+            }, {
+              { name = 'cmdline' }
+            }),
+            matching = { disallow_symbol_nonprefix_matching = false }
+          })
+        '';
       }
       {
         # catpuccin
